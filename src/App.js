89128e3fc720d822c306bf7bc7f7f47b294366ba
Youta <Youta@MacBook-Pro-4.local>
2019-10-06 13:16:02 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 // import './App.css';
 
 
-class App extends Component{
-  render(){
-    const dom = <h1 className="foo">hello</h1>;
-    return dom;
+// class App extends Component{
+//   render(){
+//     const dom = <h1 className="foo">hello</h1>;
+//     return dom;
 
-  }
+//   }
 
+// }
+
+const App = () => {
+  return <div>Hi!</div>
 }
 
 export default App;
