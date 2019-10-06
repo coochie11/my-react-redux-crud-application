38ed7e6b7577029a574494178db94db232a12b07
Youta <Youta@MacBook-Pro-4.local>
2019-10-06 16:59:06 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-// import './App.css';
+import { increment, decrement } from '../actions'
 
-const App = () => (<Counter></Counter>)
 
-class Counter extends Component{
-  constructor(props){
-    super(props)
-    console.log(this.state)
-    this.state={count: 0}
-  }
-
-  handlePlusButton = () => {
-    console.log("handlePlusButton begins")
-    this.setState({count: this.state.count +1 })
-  }
-
-  handleMinusButton = () => {
-    console.log("handleMinusButton begins")
-    this.setState({count: this.state.count -1 })
-  }
-
+class App extends Component{
   render(){
+    const props = this.props
+
     return(
       <React.Fragment>
-        <div>count:{this.state.count}</div>
-        <button onClick={this.handlePlusButton}>fuck up</button>
-        <button onClick={this.handleMinusButton}>fuck down</button>
+        <div>count:{props.value }</div>
+        <button onClick={props.increment}>fuck up</button>
+        <button onClick={props.decrement}>fuck down</button>
       </React.Fragment>
     )
   }
-
-
-
 }
 
+const mapStateToProps = state => ({value: state.count.value})
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement())
+})
+
+// const mapDispatchToProps = ({ increment, decrement })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
 
-export default App;
+// export default App;
